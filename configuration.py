1213f084ed6d445f52d77111a6f80b23cdcad6f8
Jose Perez <joseabelardo19@.gmail.com>
2025-07-14 22:41:53 -0400

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-163f9dbe-0ecc-45a2-bedd-a71ed3167f40.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
